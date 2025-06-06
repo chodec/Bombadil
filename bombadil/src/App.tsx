6db9eec43b7f1f6442b840e5c0a19198b85bfdb3
chodec <dom.lepizmo@email.cz>
2025-06-06 17:32:33 +0200

    dark mode

--- bombadil/src/App.tsx
+++ bombadil/src/App.tsx
@@ -1,15 +1,17 @@
+import { ThemeProvider } from "@/components/ui/theme-provider"
 import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1 className="text-5xl font-bold underline">
         Hello world!
       </h1>
       <div className="flex min-h-svh flex-col items-center justify-center">
         <Button>Click me</Button>
       </div>
-    </>
+    </ThemeProvider>
+
   );
 }
 
